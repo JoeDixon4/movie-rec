c2355e29dd38c1093f5cc77da3c0dd4556106d0d
Joe Dixon <joedixon4@outlook.com>
2022-11-15 12:19:23 +0000

    fixing code smells with imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
 
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
